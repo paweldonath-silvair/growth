aee2fc0b6467cefc16334bf92facfe01f3f07c2e
Pawel Donath <pawel.donath@silvair.com>
2020-03-12 10:44:51 +0100

    Presentation

--- spark/spark_scala/src/main/scala/com/pawdon/growth/MainApp.scala
+++ spark/spark_scala/src/main/scala/com/pawdon/growth/MainApp.scala
@@ -4,5 +4,5 @@ import org.apache.spark.sql.SparkSession
 
 object MainApp extends App {
   val spark = SparkSession.builder.master("local").appName("My App").getOrCreate()
-  DatasetRelation.capitolToCountryDensityRatio(spark)
+  RddRelation.capitolToCountryDensityRatio(spark)
 }
