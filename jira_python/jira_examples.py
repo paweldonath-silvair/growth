85107e07d3e91dc1d78c4608d0c3b5480f93a280
Pawel Donath <pawel.donath@silvair.com>
2020-03-18 19:24:42 +0100

    More jira functions

--- jira_python/jira_examples.py
+++ jira_python/jira_examples.py
@@ -1,6 +1,19 @@
 from jira import JIRA
 
 
+BOARDS_ID = {
+    "Platform board": 180,
+    "Platform Mobile": 198
+}
+
+
+def get_board_id(jira: JIRA, name: str):
+    for board in jira.boards():
+        if board.name == name:
+            return board.id
+    raise ValueError("Board name not found")
+
+
 def get_auth_jira(dirname="secret"):
     with open(f"{dirname}/jira_user", "r") as f:
         user = f.read()
@@ -22,6 +35,8 @@ def get_issue_info(issue, name_to_id):
         "Key",
         "Summary",
         "Status",
+        "Resolved",
+        "Issue Type",
         "Story Points",
         "Project",
         "Labels",
@@ -39,6 +54,14 @@ def get_issue_info(issue, name_to_id):
     return result
 
 
+def search_issues(jira: JIRA):
+    for issue in jira.search_issues('project=SP and component=Delta and sprint="Delta 2020 week 12" and "Epic Link"="blueZ"', maxResults=50):
+        print('{}: {}'.format(issue.key, issue.fields.summary))
+
+    print(jira.project_components("SP"))
+    print(jira.sprints(board_id=180, state="active"))
+
+
 def test():
     jira = get_auth_jira()
 
@@ -46,12 +69,14 @@ def test():
     name_to_id = {field['name']: field['id'] for field in fields}
     id_to_name = {field['id']: field['name'] for field in fields}
 
-    ticket = 'SP-6642'
+    ticket = 'SP-7076'
     issue = jira.issue(ticket)
     summary = issue.fields.summary
 
     for k, v in get_issue_info(issue, name_to_id).items():
         print(k, v)
+
+    search_issues(jira)
 
 
 if __name__ == "__main__":
